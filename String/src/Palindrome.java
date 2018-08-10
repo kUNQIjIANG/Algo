6105f932ebcdfb1cf21e51f49aaa3ed454a5c6ae
kunqi <ucabkj0@ucl.ac.uk>
2018-08-11 00:05:18 +0100

    palindrome

---
+++ String/src/Palindrome.java
@@ -0,0 +1,62 @@
+/**
+ * Created by kunqi
+ * ON 8/10/18 11:31 PM
+ */
+
+public class Palindrome {
+
+    // brute-force : O(n^3)
+    static boolean isPalindrome(String s, int left, int right){
+        while(left <= right){
+            if (s.charAt(left) == s.charAt(right)){
+                left ++;
+                right --;
+            }else{
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static int maxPalindrome(String s){
+        int longest = 0;
+        for (int i = 0; i < s.length(); i++){
+            for (int j = i+1; j < s.length(); j++){
+                if (isPalindrome(s,i,j)){
+                    longest = j-i+1 > longest ? j-i+1 : longest;
+                }
+            }
+        }
+        return longest;
+    }
+
+    // 中心扩展法
+    static int longestCenter(String s, int mid){
+        int left = mid - 1;
+        int right = mid + 1;
+        while(s.charAt(mid) == s.charAt(right)){
+            right ++;
+        }
+        while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)){
+            left --;
+            right ++;
+        }
+        return right - left - 1;
+    }
+
+    static int longestPalindrome(String s){
+        int longest = 0;
+        for (int i = 0; i < s.length(); i++){
+            int temp = longestCenter(s,i);
+            longest = temp > longest ? temp : longest;
+        }
+        return longest;
+    }
+
+    // Dynamic Programing : O(n^2)
+    static int DpPalindrome(String s){
+        int[][] dp = new int[100][100];
+        return 1;
+    }
+
+}
