91da36de95736404af2885a38ee07dfa1e86dad2
kunqi <ucabkj0@ucl.ac.uk>
2019-04-05 18:15:45 +0800

    k inverse linked list

--- LinkedList/src/KInverse.java
+++ LinkedList/src/KInverse.java
@@ -12,7 +12,6 @@ public class KInverse {
     private static Node kInverse(Node head, int k){
 
         if( head != null && head.next != null && k > 1){
-
             Node pre = null;
             Node cur = head;
             Node start = null;
@@ -78,6 +77,32 @@ public class KInverse {
 
         }
         return head;
+    }
+
+    // leetcode25 :
+    private ListNode reverseKGroup(ListNode head, int k) {
+        if (head == null) return head;
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode pre = dummy, cur = dummy, next = dummy;
+        int count = 0;
+        while(cur.next != null){
+            cur = cur.next;
+            count++;
+        }
+        while(count >= k){
+            cur = pre.next;
+            next = cur.next;
+            for(int i = 1; i < k; i++){
+                cur.next = next.next;
+                next.next = pre.next;
+                pre.next = next;
+                next = cur.next;
+            }
+            pre = cur;
+            count -= k;
+        }
+        return dummy.next;
     }
 
     public static void main(String[] args) {
